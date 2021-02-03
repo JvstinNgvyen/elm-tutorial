41585a585f88f2efe06214f31ae5dbd5454a6e4d
Justin Nguyen <nguyenj32@wit.edu>
2021-02-03 00:13:32 -0500

    [Operator] Pipe Operator, Backward Operator

--- src/Playground.elm
+++ src/Playground.elm
@@ -17,8 +17,27 @@ computeSpeed distance time =
 computeTime startTime endTime =
     endTime - startTime
 
+add a b =
+    a + b
+
+multiply c d =
+    c * d
+
+divide e f =
+    e / f
+
 main =
-    computeTime 2 3
+    Html.text <| String.fromFloat <| add 5 <| multiply 10 <| divide 30 10
+    --Html.text (String.fromFloat (add 5 (multiply 10 (divide 30 10))))
+    {--divide 30 10
+        |> multiply 10
+        |> add 5
+        |> String.fromFloat
+        |> Html.text
+    --}
+    {--
+      computeTime 2 3
         |> computeSpeed 7.67
         |> escapeEarth 11
         |> Html.text
+    --}
